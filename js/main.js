dafa86a6aa0f226251494defbf8d1a4240603086
Sanjay-creator-dev <sanjaysir4676@gmail.com>
2024-09-22 19:27:25 +0530

    hello

--- js/main.js
+++ js/main.js
@@ -1,74 +1,74 @@
-$(document).ready(function(){
-// Hero Slider
-$('#hero-slider').owlCarousel({
-    loop:true,
-    margin:0,
-    nav: true,
-    items: 1,
-    dots: false,
-    smartSpeed: 1000, 
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: false,
-    responsive:{
-        0:{
-           nav:false
-        },
-       768:{
-        nav: true
-       },
-    }
-})
-// OUR DIVERSIFIED TALENTS SLIDER
-$('#talent-slider').owlCarousel({
-    loop:true,
-    margin:0,
-    nav: true,
-    dots: true,
-    smartSpeed: 1000,
-    margin: 34,
-    responsive:{
-        0:{
-          items: 1,
-        },
-       768:{
-        items: 3,
-       },
-       1140:{
-       items: 3,
-       center: true,
-       }
-    }
-})
+$(document).ready(function () {
+    // Hero Slider
+    $('#hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                nav: false
+            },
+            768: {
+                nav: true
+            },
+        }
+    })
+    // OUR DIVERSIFIED TALENTS SLIDER
+    $('#talent-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: true,
+        smartSpeed: 1000,
+        margin: 34,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 3,
+            },
+            1140: {
+                items: 3,
+                center: true,
+            }
+        }
+    })
 
 
-// Testimonial
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin: 10,
-    nav: false,
-    dots: true,
-    items: 1,
-    smartSpeed: 800,
-})
+    // Testimonial
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 1,
+        smartSpeed: 800,
+    })
 });
 
 
 // counter design
-document.addEventListener("DOMContentLoaded",()=>{
-    function counter(id, start, end, duration){
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
         let obj = document.getElementById(id),
-        current = start,
-        range = end - start,
-        increament = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration / range)),
-        timer = setInterval(()=>{
-            current += increament;
-            obj.textContent = current;
-            if(current == end){
-                clearInterval(timer);
-            }
-        }, step);
+            current = start,
+            range = end - start,
+            increament = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increament;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
     }
     counter("count1", 0, 26, 3000);
     counter("count2", 0, 1000, 3000);
